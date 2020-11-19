0daf42ce50d6e1b641a7b2ff5a6d7144310c0ca6
Hunter <kolasa.hunter@aol.com>
2020-11-19 09:32:45 -0500

    commit tree 197dd2deb7050ca4c4a17f15fe638899ff8b68aa

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+    attr_accessor :roster, :student_name, :school_name
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+    def add_student(student_name, grade)
+        if @roster.key?(grade)
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        sorted_roster = {}
+        @roster.keys.sort.each do |key|
+            sorted_roster[key] = []
+            sorted_roster[key] << @roster[key].flatten.sort
+        end
+        sorted_roster.keys.each do |key|
+            sorted_roster[key].flatten!
+        end
+        sorted_roster
+    end
+end
